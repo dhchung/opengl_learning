f9cb50291c50b986e2bceb680fa8d0a70c188f7e
Dongha Chung <haetae.dongha@gmail.com>
2020-02-26 14:49:34 +0900

    textures

--- src/hellotexture/learnopengl_texture_4.cpp
+++ src/hellotexture/learnopengl_texture_4.cpp
@@ -4,6 +4,10 @@
 #include "shader.h"
 #include <math.h>
 #include "stb_image.h"
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) { // Change viewport if the user change the size of the window
     glViewport(0, 0, width, height);
 }
@@ -24,7 +28,7 @@ int main() {
     //Making window
     GLFWwindow * window = glfwCreateWindow(800,600, "LearnOpenGL", NULL, NULL);
     if(window == NULL) {
-        std::cout << "Failed to create GLFW window" << std::endl;
+        std::cout << "Failed to create GLFW wi3ndow" << std::endl;
         glfwTerminate();
         return -1;
     }
@@ -40,17 +44,10 @@ int main() {
     //Shaders
     Shader ourShader("shaders/shader.vs", "shaders/shader.fs");
 
-
-    //Texture
-    float texCoords[] = {
-        0.0f, 0.0f,
-        1.0f, 0.0f,
-        0.5f, 1.0f
-    };
-
-    unsigned int texture;
-    glGenTextures(1, &texture);
-    glBindTexture(GL_TEXTURE_2D, texture);
+    unsigned int texture1, texture2;
+    glGenTextures(1, &texture1);
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, texture1);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
@@ -67,6 +64,26 @@ int main() {
     }
     stbi_image_free(data);
 
+    glGenTextures(1, &texture2);
+    glActiveTexture(GL_TEXTURE1);
+    glBindTexture(GL_TEXTURE_2D, texture2);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    stbi_set_flip_vertically_on_load(true);
+
+    data = stbi_load("assets/awesomeface.png", &width, &height, &nrChannels, 0);
+    if(data) {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }else {
+        std::cout << "Failed to load texture" << std::endl;
+
+    }
+    stbi_image_free(data);
+
 
     float vertices[] = {    //Colors          //Texture Coordinate
         -0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f,
@@ -74,14 +91,22 @@ int main() {
          0.0f,  0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.5f, 1.0f
     };
 
-    unsigned int VBO, VAO;
+    unsigned int indices[] = {
+        0,1,2
+    };
+
+    unsigned int VBO, VAO, EBO;
     glGenBuffers(1, &VBO);
     glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &EBO);
     
     glBindVertexArray(VAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8*sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
@@ -95,20 +120,42 @@ int main() {
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
 
+
+    //Transformation
+    glm::mat4 trans = glm::mat4(1.0f);
+    unsigned int transformLoc = glGetUniformLocation(ourShader.ID, "transform");
+    // glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(trans));
+
     // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
     glViewport(0,0,800,600); // Setup the viewport size (location of the viewport in the window, width and height of the window)
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
 
+    ourShader.use();
+    ourShader.setInt("texture1",0);
+    ourShader.setInt("texture2",1);
+
     while(!glfwWindowShouldClose(window)) { //Check if there's any command to close the window to GLFW
         processInput(window);
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+        
+        trans = glm::mat4(1.0f);
+        trans = glm::translate(trans, glm::vec3(0.5f, -0.5f, 0.0f));
+        trans = glm::rotate(trans, (float)glfwGetTime(), glm::vec3(0.0f, 0.0f, 1.0f));        
+        glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(trans));
+        
         ourShader.use();
-        glBindTexture(GL_TEXTURE_2D, texture);
         glBindVertexArray(VAO);
         glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        trans = glm::mat4(1.0f);
+        trans = glm::translate(trans, glm::vec3(-0.5f, 0.5f, 0.0f));
+        trans = glm::scale(trans, glm::vec3(sin(glfwGetTime())));
+        glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(trans));
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        
         glBindVertexArray(0);
         glfwSwapBuffers(window); //e.g. double buffer -> Swap the color buffer
         glfwPollEvents(); // Check any events(keyboard or mouse), update the window status, and callback the functions        
@@ -116,6 +163,7 @@ int main() {
 
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
 
     glfwTerminate();
     return 0;
